1976e7ba9c272d82a0cbc6a53cb780d3320d9aae
itsyaboikris <kristoff_riley@outlook.com>
2023-04-21 20:36:39 -0400

    sticky header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,28 +7,30 @@ import React from 'react'
 export default function Header() {
 	return (
 	
-		<div className='flex items-center justify-between max-w-6xl mx-4 xl:mx-auto'>
+		<div className='shadow-sm border-b sticky top-0 bg-white z-30'>
+			<div className='flex items-center justify-between max-w-6xl mx-4 xl:mx-auto'>
 
-			<div className='cursor-pointer h-24 w-24 relative hidden lg:inline-grid' >
-				<Image src={`/Instagram_logo.png`} className='object-contain' alt='logo' fill />
-			</div>
-			<div className='cursor-pointer h-24 w-10 relative lg:hidden' >
-				<Image src={`/instagram.png`} className='object-contain' alt='logo' fill />
-			</div>
-
-			<div className="relative mt-1">
-          		<div className="absolute top-2 left-2">
-					<MagnifyingGlassIcon className='h-5 text-gray-500'/>
+				<div className='cursor-pointer h-24 w-24 relative hidden lg:inline-grid' >
+					<Image src={`/Instagram_logo.png`} className='object-contain' alt='logo' fill />
 				</div>
-				<input type="text" className='bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md' placeholder='Search' />
-			</div>
+				<div className='cursor-pointer h-24 w-10 relative lg:hidden' >
+					<Image src={`/instagram.png`} className='object-contain' alt='logo' fill />
+				</div>
 
-			<div className='flex space-x-4 items-center'>
-				<HomeIcon className='hidden md:inline-flex h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out'/>
-				<PlusCircleIcon className='h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out'/>
-				<img src="/avatar.jpg" alt="User Image" className='h-10 rounded-full cursor-pointer' />
-			</div>
+				<div className="relative mt-1">
+					<div className="absolute top-2 left-2">
+						<MagnifyingGlassIcon className='h-5 text-gray-500'/>
+					</div>
+					<input type="text" className='bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md' placeholder='Search' />
+				</div>
 
+				<div className='flex space-x-4 items-center'>
+					<HomeIcon className='hidden md:inline-flex h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out'/>
+					<PlusCircleIcon className='h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out'/>
+					<img src="/avatar.jpg" alt="User Image" className='h-10 rounded-full cursor-pointer' />
+				</div>
+
+			</div>
 		</div>
 		
 	)
