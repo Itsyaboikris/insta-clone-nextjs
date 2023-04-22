75812ca6f83eac70de7bd3ca1f89e55277eaa77d
itsyaboikris <kristoff_riley@outlook.com>
2023-04-21 22:12:25 -0400

    style story section

--- src/components/Stories.jsx
+++ src/components/Stories.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import minifaker from 'minifaker'
-import 'minifaker/locales/en'
+import { faker } from '@faker-js/faker';
 import Story from './Story'
 
 export default function Stories() {
@@ -8,15 +7,22 @@ export default function Stories() {
 	const [storyUsers, setStoryUsers] = useState([])
 
 	useEffect(() => {
-		const storyUsers = minifaker.array(20, (i) => ({
-			username: minifaker.username({locale:"en"}),
-			img : `https://i.pravatar.cc/150?img=${Math.ceil(Math.random() * 1000)}`,
-			id: i
-		}))
+
+		let storyUsers = []
+		for (let i = 0; i < 20; i++) {
+			storyUsers.push({
+				username: faker.name.fullName(),
+				img : faker.image.avatar(), //`https://i.pravatar.cc/150?img=${Math.ceil(Math.random() * 70)}`,
+				id: i
+			})
+		}
+
+		setStoryUsers(storyUsers)
+
 	}, [])
 
 	return (
-		<div>
+		<div className='flex space-x-2 p-6 bg-white mt-8 border-gray-200 border overflow-x-scroll rounded-sm scrollbar-none'>
 			{
 				storyUsers.map (user => (
 					<Story key={user.id} username={user.username} img={user.img} />
