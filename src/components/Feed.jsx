00da9bc7598fa0a1b6d49c49bbe4d19411bb9ee1
itsyaboikris <kristoff_riley@outlook.com>
2023-04-22 21:15:36 -0400

    create mini profile component

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import MiniProfile from './MiniProfile'
 import Posts from './Posts'
 import Stories from './Stories'
 
@@ -12,6 +13,12 @@ export default function Feed() {
 		</section>
 	  
 		<section className='hidden md:inline-grid md:col-span-1'>
+
+			<div className='fixed w-[380px]'>
+
+				<MiniProfile />
+
+			</div>
 
 		</section>
 
