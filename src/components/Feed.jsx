3423446042dd8211921296803c0b36d20b4111f3
itsyaboikris <kristoff_riley@outlook.com>
2023-04-23 06:22:23 -0400

    included session in mini profile & story component

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -3,28 +3,32 @@ import MiniProfile from './MiniProfile'
 import Posts from './Posts'
 import Stories from './Stories'
 import Suggestions from './Suggestions'
+import { useSession } from "next-auth/react"
 
 export default function Feed() {
-  return (
-	<main className='grid grid-cols-1 md:grid-cols-3 md:max-w-6xl mx-auto'>
 
-		<section className='md:col-span-2'>
-			<Stories/>
-			<Posts/>
-		</section>
-	  
-		<section className='hidden md:inline-grid md:col-span-1'>
+	const {data: session} = useSession()
 
-			<div className='fixed w-[380px]'>
+	return (
+		<main className={`grid ${session ? "grid-cols-1 md:grid-cols-3 md:max-w-6xl mx-auto" : "grid-cols-1 md:grid-cols-2 md:max-w-3xl mx-auto" } `}>
 
-				<MiniProfile/>
+			<section className='md:col-span-2'>
+				<Stories/>
+				<Posts/>
+			</section>
+		
+			<section className='hidden md:inline-grid md:col-span-1'>
 
-				<Suggestions/>
+				<div className='fixed w-[380px]'>
 
-			</div>
+					<MiniProfile/>
 
-		</section>
+					<Suggestions/>
 
-	</main>
-  )
+				</div>
+
+			</section>
+
+		</main>
+	)
 }
--- src/components/Stories.jsx
+++ src/components/Stories.jsx
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import { faker } from '@faker-js/faker';
 import Story from './Story'
+import { useSession, signOut } from "next-auth/react"
 
 export default function Stories() {
+
+	const {data: session} = useSession()
 
 	const [storyUsers, setStoryUsers] = useState([])
 
@@ -23,6 +26,11 @@ export default function Stories() {
 
 	return (
 		<div className='flex space-x-2 p-6 bg-white mt-8 border-gray-200 border overflow-x-scroll rounded-sm scrollbar-none'>
+			{
+				session && (
+					<Story img={session.user.image} username={session.user.name} isUser="true"/>
+				)
+			}
 			{
 				storyUsers.map (user => (
 					<Story key={user.id} username={user.username} img={user.img} />
