59cf244fd6cef256a52fc9d0c6e42da16a34cc2d
itsyaboikris <kristoff_riley@outlook.com>
2023-04-23 19:50:24 -0400

    install and implement react modal

--- src/components/UploadModal.jsx
+++ src/components/UploadModal.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useRecoilState } from 'recoil'
 import {modalState} from '../atoms/modalAtom'
+import Modal from "react-modal"
 
 export default function UploadModal() {
 
@@ -8,8 +9,13 @@ export default function UploadModal() {
 
 	return (
 		<div>
-			<h1>Modal</h1>
-			{ open && <h1>open</h1>}
+			{ open && (
+				<Modal className="max-w-lg w-[90%] h-[300px] absolute top-56 left-[50%] translate-x-[-50%] bg-white border-2 rounded-md shadow-md" isOpen={open} onRequestClose={()=>setOpen(false)}>
+					<div className='flex flex-col justify-center items-center h-[100%]'>
+						<h1>modal</h1>
+					</div>
+				</Modal>
+			)}
 		</div>
 	)
 }
