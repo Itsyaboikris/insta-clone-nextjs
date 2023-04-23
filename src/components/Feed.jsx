fc3269ff48e7bb3346668055825f1e9fcd9dfa46
itsyaboikris <kristoff_riley@outlook.com>
2023-04-22 21:01:09 -0400

    added some responsiveness

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -4,14 +4,14 @@ import Stories from './Stories'
 
 export default function Feed() {
   return (
-	<main>
+	<main className='grid grid-cols-1 md:grid-cols-3 md:max-w-6xl mx-auto'>
 
-		<section className=''>
+		<section className='md:col-span-2'>
 			<Stories/>
 			<Posts/>
 		</section>
 	  
-		<section className=''>
+		<section className='hidden md:inline-grid md:col-span-1'>
 
 		</section>
 
