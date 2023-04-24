1e267797909dc0ec9908db3b9b5e52acad249fd4
itsyaboikris <kristoff_riley@outlook.com>
2023-04-23 22:57:24 -0400

    show comments in comment section

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,14 +1,28 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { EllipsisHorizontalIcon, FaceSmileIcon, HeartIcon, ChatBubbleOvalLeftIcon, BookmarkIcon } from '@heroicons/react/24/outline'
 import { useSession } from "next-auth/react"
-import { addDoc, collection, serverTimestamp } from 'firebase/firestore'
+import { addDoc, collection, onSnapshot, orderBy, query, serverTimestamp } from 'firebase/firestore'
 import { db } from "../firebase"
+import Moment from 'react-moment';
 
 export default function Post({id, img, userImg, caption, username }) {
 	
 	const {data: session} = useSession()
 
 	const [comment, setComment] = useState("")
+	const [comments, setComments] = useState([])
+
+	useEffect(() => {
+		const unsubscribe = onSnapshot(
+			query(collection(db, "posts", id, "comments"), orderBy("timestamp","desc")), (snapshot) => {
+				setComments(snapshot.docs)
+				console.log("snapshot.docs")
+				console.log(snapshot.docs)
+				console.log("comments")
+				console.log(comments)
+			}
+		)
+	}, [db, id])
 
 	async function sendComment(e) {
 		e.preventDefault();
@@ -53,6 +67,23 @@ export default function Post({id, img, userImg, caption, username }) {
 				<span className='font-bold mr-2 '>{username}</span>
 				{caption}
 			</p>
+
+			{
+				(comments.length > 0 )&& (
+					<div className='mx-10 max-h-24 overflow-y-scroll scrollbar-none'>
+						{
+							comments.map((c) => (
+								<div className='flex items-center space-x-2 mb-2'>
+									<img className='h-7 rounded-full object-cover' src={c.data().userImage} alt="user-image" />
+									<p className='font-semibold '>{c.data().username}</p>
+									<p className='flex-1 truncate'>{c.data().comment}</p>
+									<Moment fromNow>{c.data().timestamp?.toDate()}</Moment>
+								</div>
+							))
+						}
+					</div>
+				)
+			}
 
 			{
 				session && (
